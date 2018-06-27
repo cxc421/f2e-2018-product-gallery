7abb2c21917ed3471f199cfabf43174f46ac6bad
Chris Chuang <chrischuang0421@gmail.com>
2018-06-28 00:54:06 +0800

    Partial work of product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,8 @@ import GridLines from 'components/GridLines';
 import Logo from 'components/Logo';
 
 import IMG_6 from 'images/product-6.jpg';
+import IMG_6_1 from 'images/product-6-1.jpg';
+import IMG_6_2 from 'images/product-6-2.jpg';
 
 export default class Product extends React.Component {
 
@@ -30,9 +32,23 @@ export default class Product extends React.Component {
           <div className="text text-4">TOP</div>
         </div>
 
-        <div className="product-block-2"></div>
+        <div className="product-block-2">
+          <h1 className="title-1">VINTAGE DENIM</h1>
+          <h2 className="title-2">Kogi Cosby sweater ethical squid irony</h2>
+          <h3 className="title-3">PRODUCT DETAILS</h3>
+          <p className="content">
+          In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew. Now hands are wanted here, and then again hands are wanted there. There is no staying in any one place; for at one and the same time everything has to be done everywhere. It is much the same with him who endeavors the description of the scene. 
+  <br/>
+            <br />
+  We must now retrace our way a little. It was mentioned that upon first breaking ground in the whale's back, the blubber-hook was inserted into the original hole there cut by the spades of the mates. But how did so clumsy and weighty a mass as that same hook get fixed in that hole? It was inserted there by my particular friend Queequeg, whose duty it was, as harpooneer.
+          </p>
+        </div>
 
-        <div className="product-block-3"></div>
+        <div className="product-block-3">
+          <div className="img-area img-1">
+            <img src={IMG_6_2} alt="image 6-2"/>
+          </div>
+        </div>
 
       </div>
     );
